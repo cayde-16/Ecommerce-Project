638f148db6940e9bed3fe9e5c73225aa89e681d8
Cade Heck <cadeheck16@gmail.com>
2022-07-16 20:52:43 -0400

    Finished Phase 1 of project

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)

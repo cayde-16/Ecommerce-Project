58fa15f9edb74982b82767611b528cb097165935
Cade Heck <cadeheck16@gmail.com>
2022-07-17 17:48:17 -0400

    Started implementing products in database and render it out though backend and shown through templates

--- base/views.py
+++ base/views.py
@@ -4,11 +4,12 @@ from . models import *
 # Create your views here.
 
 def main(request):
-    products = Product.objects.all()
-    context = {'products': products}
+    products = Product.objects.all()[0:3]
+    latest_products = Product.objects.all().reverse()[3:9]
+    context = {'latest_products':latest_products,'products': products}
     return render(request, 'base/main.html', context)
 
-    
+
 def product(request):
     context = {}
     return render(request,'base/products.html', context)
